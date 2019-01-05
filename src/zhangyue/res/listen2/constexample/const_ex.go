4f764dc8c46127614adaaf071464ac4195742214
chentaizhang <492745473@qq.com>
2019-01-05 22:37:14 +0800

    常量练习

---
+++ src/zhangyue/res/listen2/constexample/const_ex.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+
+const (
+	a = iota
+	b
+	c
+	d
+	e = 8
+	f
+	g = iota
+	h
+	k
+)
+
+
+const (
+	A = iota
+	B
+	C
+)
+
+
+func main(){
+	fmt.Printf("a= %d\n", a)
+	fmt.Printf("b= %d\n", b)
+	fmt.Printf("c= %d\n", c)
+	fmt.Printf("d= %d\n", d)
+	fmt.Printf("e= %d\n", e)
+	fmt.Printf("f= %d\n", f)
+	fmt.Printf("g= %d\n", f)
+	fmt.Printf("h= %d\n", h)
+	fmt.Printf("k= %d\n", k)
+	fmt.Printf("A= %d\n", A)
+	fmt.Printf("B= %d\n", B)
+    fmt.Printf("C= %d\n", C)
+}
+
+/*
+const() 中 如果 定义了 常量 不给定默认值 那么 该常量的表达式 和 其上一个 表达式是相同的
+iota 在const 中 没经过一层 自增1
+*/
